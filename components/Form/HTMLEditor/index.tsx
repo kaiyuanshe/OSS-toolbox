cede61be0aafbd990be4aed54b79de795bef3b9f
luojiyin <luojiyin@hotmail.com>
2024-07-02 23:36:44 +0800

    add HTMLEditor compents

---
+++ components/Form/HTMLEditor/index.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import {
+  AudioTool,
+  Editor,
+  EditorProps,
+  IFrameTool,
+  ImageTool,
+  OriginalTools,
+  VideoTool,
+} from 'react-bootstrap-editor';
+import { insertToCursor, parseDOM } from 'web-utility';
+
+import fileStore from '../models/Base/File';
+
+ImageTool.prototype.save = blob =>
+  fileStore.upload(new File([blob], uniqueID()));
+
+const ExcludeTools = [IFrameTool, AudioTool, VideoTool];
+
+const CustomTools = OriginalTools.filter(
+  Tool => !ExcludeTools.includes(Tool as Constructor<IFrameTool>),
+);
+
+const HTMLEditor: FC<EditorProps> = props => (
+  <Editor tools={CustomTools} {...props} />
+);
+export default HTMLEditor;
